dac0daa8c40367d8b2b9deb8c5f2bdc7d33dfbcd
Guo <74940716+965694547@users.noreply.github.com>
2022-06-25 13:41:05 +0800

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env/python3
+import os
+import sys
+import speechbrain as sb
+from speechbrain.utils.distributed import run_on_main
+from hyperpyyaml import load_hyperpyyaml
+from utils.model import get_model
+
+
+def dataio_prep(hparams):
+    """Creates data processing pipeline"""
+
+    # 1. Define datasets
+    train_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["train_data"],
+        replacements={"data_root": hparams["data_folder"]},
+    )
+
+    valid_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["valid_data"],
+        replacements={"data_root": hparams["data_folder"]},
+    )
+
+    test_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["test_data"],
+        replacements={"data_root": hparams["data_folder"]},
+    )
+
+    datasets = [train_data, valid_data, test_data]
+
+    # 2. Provide audio pipelines
+
+    @sb.utils.data_pipeline.takes("mix_wav")
+    @sb.utils.data_pipeline.provides("mix_sig")
+    def audio_pipeline_mix(mix_wav):
+        mix_sig = sb.dataio.dataio.read_audio(mix_wav)
+        return mix_sig
+
+    @sb.utils.data_pipeline.takes("s1_wav")
+    @sb.utils.data_pipeline.provides("s1_sig")
+    def audio_pipeline_s1(s1_wav):
+        s1_sig = sb.dataio.dataio.read_audio(s1_wav)
+        return s1_sig
+
+    @sb.utils.data_pipeline.takes("s2_wav")
+    @sb.utils.data_pipeline.provides("s2_sig")
+    def audio_pipeline_s2(s2_wav):
+        s2_sig = sb.dataio.dataio.read_audio(s2_wav)
+        return s2_sig
+
+    if hparams["num_spks"] == 3:
+
+        @sb.utils.data_pipeline.takes("s3_wav")
+        @sb.utils.data_pipeline.provides("s3_sig")
+        def audio_pipeline_s3(s3_wav):
+            s3_sig = sb.dataio.dataio.read_audio(s3_wav)
+            return s3_sig
+
+    if hparams["use_wham_noise"]:
+
+        @sb.utils.data_pipeline.takes("noise_wav")
+        @sb.utils.data_pipeline.provides("noise_sig")
+        def audio_pipeline_noise(noise_wav):
+            noise_sig = sb.dataio.dataio.read_audio(noise_wav)
+            return noise_sig
+
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_mix)
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_s1)
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_s2)
+    if hparams["num_spks"] == 3:
+        sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_s3)
+
+    if hparams["use_wham_noise"]:
+        print("Using the WHAM! noise in the data pipeline")
+        sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline_noise)
+
+    if (hparams["num_spks"] == 2) and hparams["use_wham_noise"]:
+        sb.dataio.dataset.set_output_keys(
+            datasets, ["id", "mix_sig", "s1_sig", "s2_sig", "noise_sig"]
+        )
+    elif (hparams["num_spks"] == 3) and hparams["use_wham_noise"]:
+        sb.dataio.dataset.set_output_keys(
+            datasets,
+            ["id", "mix_sig", "s1_sig", "s2_sig", "s3_sig", "noise_sig"],
+        )
+    elif (hparams["num_spks"] == 2) and not hparams["use_wham_noise"]:
+        sb.dataio.dataset.set_output_keys(
+            datasets, ["id", "mix_sig", "s1_sig", "s2_sig"]
+        )
+    else:
+        sb.dataio.dataset.set_output_keys(
+            datasets, ["id", "mix_sig", "s1_sig", "s2_sig", "s3_sig"]
+        )
+
+    return train_data, valid_data, test_data
+
+
+if __name__ == "__main__":
+
+    # Load hyperparameters file with command-line overrides
+    hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin, overrides)
+
+    hparams["test_only"] = True
+    hparams["dataloader_opts"]["batch_size"] = 1
+
+    # Initialize ddp (useful only for multi-GPU DDP training)
+    sb.utils.distributed.ddp_init_group(run_opts)
+
+
+    # Create experiment directory
+    sb.create_experiment_directory(
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file,
+        overrides=overrides,
+    )
+
+    # Check if wsj0_tr is set with dynamic mixing
+    if hparams["dynamic_mixing"] and not os.path.exists(
+        hparams["base_folder_dm"]
+    ):
+        print(
+            "Please, specify a valid base_folder_dm folder when using dynamic mixing"
+        )
+        sys.exit(1)
+
+    # Data preparation
+    from recipes.LibriMix.prepare_data import prepare_librimix
+
+    run_on_main(
+        prepare_librimix,
+        kwargs={
+            "datapath": hparams["data_folder"],
+            "savepath": hparams["save_folder"],
+            "n_spks": hparams["num_spks"],
+            "skip_prep": hparams["skip_prep"],
+            "librimix_addnoise": hparams["use_wham_noise"],
+            "fs": hparams["sample_rate"],
+        },
+    )
+
+    # Create dataset objects
+    if hparams["dynamic_mixing"]:
+        from dynamic_mixing import (
+            dynamic_mix_data_prep_librimix as dynamic_mix_data_prep,
+        )
+
+        # if the base_folder for dm is not processed, preprocess them
+        if "processed" not in hparams["base_folder_dm"]:
+            # if the processed folder already exists we just use it otherwise we do the preprocessing
+            if not os.path.exists(
+                os.path.normpath(hparams["base_folder_dm"]) + "_processed"
+            ):
+                from recipes.LibriMix.meta.preprocess_dynamic_mixing import (
+                    resample_folder,
+                )
+
+                print("Resampling the base folder")
+                run_on_main(
+                    resample_folder,
+                    kwargs={
+                        "input_folder": hparams["base_folder_dm"],
+                        "output_folder": os.path.normpath(
+                            hparams["base_folder_dm"]
+                        )
+                        + "_processed",
+                        "fs": hparams["sample_rate"],
+                        "regex": "**/*.flac",
+                    },
+                )
+                # adjust the base_folder_dm path
+                hparams["base_folder_dm"] = (
+                    os.path.normpath(hparams["base_folder_dm"]) + "_processed"
+                )
+            else:
+                print(
+                    "Using the existing processed folder on the same directory as base_folder_dm"
+                )
+                hparams["base_folder_dm"] = (
+                    os.path.normpath(hparams["base_folder_dm"]) + "_processed"
+                )
+
+        train_data = dynamic_mix_data_prep(hparams)
+        _, valid_data, test_data = dataio_prep(hparams)
+    else:
+        train_data, valid_data, test_data = dataio_prep(hparams)
+
+    # Load pretrained model if pretrained_separator is present in the yaml
+    if not "pretrained_separator" in hparams:
+        run_on_main(hparams["pretrained_separator"].collect_files)
+        hparams["pretrained_separator"].load_collected()
+
+    # Brain class initialization
+    separator = get_model(hparams, run_opts)
+
+    # re-initialize the parameters if we don't use a pretrained model
+    if "pretrained_separator" not in hparams:
+        for module in separator.modules.values():
+            separator.reset_layer_recursively(module)
+
+
+    # Eval
+    separator.evaluate(test_data, min_key="si-snr")
+    separator.save_results(test_data)
